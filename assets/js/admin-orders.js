e735ba68788cf24f190ac876a134e396a29036c5
JoveMoniz <kufongo@gmail.com>
2026-02-08 18:57:06 +0000

    Initial commit frontend

--- assets/js/admin-orders.js
+++ assets/js/admin-orders.js
@@ -156,7 +156,7 @@ document.getElementById("ordersTable").addEventListener("click", async (e) => {
 
     /* -------- BUILD ROW -------- */
     detailsRow = document.createElement("tr");
-    detailsRow.className = "order-details-row open";
+    detailsRow.className = "order-details-row";
 
 
     const cell = document.createElement("td");
@@ -218,6 +218,8 @@ document.getElementById("ordersTable").addEventListener("click", async (e) => {
 
     detailsRow.appendChild(cell);
     row.after(detailsRow);
+    detailsRow.className = "order-details-row";
+
     return;
   }
 
